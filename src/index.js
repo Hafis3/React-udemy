8c907ddaa95ec4201f9279c651c212951fd0cf13
Hafis3 <hafis3@gmail.com>
2021-05-24 16:40:59 +0530

    React-398

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-//Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
 import React from "react";
 import ReactDom from "react-dom";
+
 const name = "Hafis";
+const data = new Date().getFullYear();
+const imgg = "https://picsum.photos/200";
 ReactDom.render(
   <div>
-    <h2>Fruits {name}</h2>
+    <h2 className="heading">Fruits {name}</h2>
     <ul>
-      <li>First </li>
+      <img src={imgg} alt="img" />
+      <li>{data} </li>
       <li>Second </li>
       <li>Third </li>
       <li>Fourth </li>
