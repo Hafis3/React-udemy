df0f3cddd3605dcf6bc643b37a3a0dcf8d9937ec
Hafis3 <hafis3@gmail.com>
2021-05-24 11:42:34 +0530

    Head Added

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
-import React from 'react';
-import ReactDom from 'react-dom'
-
+import React from "react";
+import ReactDom from "react-dom";
 
 ReactDom.render(
-  <ul>
-  <li>First </li>
-  <li>Second </li>
-  <li>Third </li>
-  <li>Fourth </li>
-</ul>,
-document.getElementById("root")
+  <div>
+    <h2>Fruits</h2>
+    <ul>
+      <li>First </li>
+      <li>Second </li>
+      <li>Third </li>
+      <li>Fourth </li>
+    </ul>
+  </div>,
+  document.getElementById("root")
 );
