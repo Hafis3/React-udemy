915258a1ef174298b2b1e3be963aa9f2fefd237f
Hafis3 <hafis3@gmail.com>
2021-05-24 18:06:39 +0530

    React-399

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from "react";
 import ReactDom from "react-dom";
+let salutation;
+const dynamicCss = {
+  color: ""
+};
+const hour = new Date(2021, 5, 24, 19).getHours();
+console.log(hour);
 
-const name = "Hafis";
-const data = new Date().getFullYear();
-const imgg = "https://picsum.photos/200";
+if (hour < 12) {
+  dynamicCss.color = "green";
+  salutation = "Good Morning";
+} else if (hour < 18) {
+  dynamicCss.color = "blue";
+  salutation = "Good After Noon";
+} else {
+  salutation = "Good Evening";
+  dynamicCss.color = "red";
+}
+
 ReactDom.render(
-  <div>
-    <h2 className="heading">Fruits {name}</h2>
-    <ul>
-      <img src={imgg} alt="img" />
-      <li>{data} </li>
-      <li>Second </li>
-      <li>Third </li>
-      <li>Fourth </li>
-    </ul>
-  </div>,
+  <h1 style={dynamicCss}> {salutation} </h1>,
   document.getElementById("root")
 );
