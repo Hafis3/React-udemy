25de384c7b55fc3c2434dcca8af742b6d2266be1
Hafis3 <hafis3@gmail.com>
2021-05-25 09:21:07 +0530

    React-403-Calculator

---
+++ src/Calculator.jsx
@@ -0,0 +1,14 @@
+const Add = (firstNumber, secondNumber) => {
+  return firstNumber + secondNumber;
+};
+const Subtract = (firstNumber, secondNumber) => {
+  return firstNumber - secondNumber;
+};
+const Multiply = (firstNumber, secondNumber) => {
+  return firstNumber * secondNumber;
+};
+const Divide = (firstNumber, secondNumber) => {
+  return firstNumber / secondNumber;
+};
+
+export { Add, Subtract, Multiply, Divide };
--- src/List.jsx
+++ src/List.jsx
@@ -1,13 +1,13 @@
 import React from "react";
+import { Add, Subtract, Multiply, Divide } from "./Calculator";
 
 export default function List() {
   return (
     <ul>
-      <li>Item 1-> 5</li>
-      <li>Item 1-> 5</li>
-      <li>Item 1-> 5</li>
-      <li>Item 1-> 5</li>
-      <li>Item 1-> 5</li>
+      <li>{Add(5, 5)}</li>
+      <li>{Subtract(5, 5)}</li>
+      <li>{Multiply(5, 5)}</li>
+      <li>{Divide(5, 5)}</li>
     </ul>
   );
 }
